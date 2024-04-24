572c14833d194e2f0c93235010ab90bb5832aa69
Jeremiah Olisa <31079573+jeremiah-olisa@users.noreply.github.com>
2024-04-24 15:38:40 +0100

    Update ftp-deploy.yaml
    Update Donation.php
    
    push vendor

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Model for donation
 class Donation extends Model
 {
     use HasFactory;
